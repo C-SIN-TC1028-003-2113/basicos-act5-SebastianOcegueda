10e05931c04b1483a7cdf538cb808fc5b6549cb0
SebastianOcegueda <A01742393@itesm.mx>
2021-08-17 14:43:53 +0000

    Resuelta la actividad SIN

--- assignments/00Escalera/src/exercise.py
+++ assignments/00Escalera/src/exercise.py
@@ -1,5 +1,15 @@
+import math
 def main():
-    pass
+    altura=float(input("Altura de la casa: "))
+    angulo=int(input("Angulo en grados: "))
+    
+    aradians=math.radians(angulo)
+
+    largo=round(altura/math.sin(aradians))
+
+    print(f"Largo de la escalera: {largo}")
+
 
 if __name__ == '__main__':
     main()
+
